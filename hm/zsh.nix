885d602bed8f567bad9f7a3ea08fc2c4115325d1
amwo <tsushimori@gmail.com>
2025-01-02 15:36:18 +0900

    init

--- hm/zsh.nix
+++ hm/zsh.nix
@@ -33,15 +33,17 @@
       export EDITOR="nvim"
       export NIXPKGS_ALLOW_UNFREE=1
       export TERM=xterm-256color
-      export PROMPT="${PWD##*/} ~ "
 
       autoload -Uz vcs_info
-      precmd() { vcs_info; }
-      setopt prompt_subst
+      precmd() { vcs_info }
 
       zstyle ':vcs_info:*' enable git
       zstyle ':vcs_info:git:*' formats '%b'
       zstyle ':vcs_info:git:*' actionformats '%b|%a'
+
+      setopt PROMPT_SUBST
+
+      export PROMPT="%F{green}%*%f %F{blue}%~%f %F{red}%f$ "
     '';
   };
 }
