980bced753812c40f6ea0e3eecc611f74eb9a54c
amwo <tsushimori@gmail.com>
2025-01-02 15:03:30 +0900

    init

--- hm/zsh.nix
+++ hm/zsh.nix
@@ -29,22 +29,31 @@
       grep = "rg";
     };
 
-    initExtra =
-      ''
-        export EDITOR="nvim"
-        export NIXPKGS_ALLOW_UNFREE=1
-        export TERM=xterm-256color
+    initExtra = ''
+      export EDITOR="nvim"
+      export NIXPKGS_ALLOW_UNFREE=1
+      export TERM=xterm-256color
 
-        autoload -Uz vcs_info
-        precmd() { vcs_info }
-        setopt prompt_subst
+      # PATH設定
+      export PATH="$HOME/bin:$PATH"
 
-        zstyle ":vcs_info:git:*" formats " (%b)"
-        zstyle ":vcs_info:*" enable git
+      # vcs_info 初期化
+      autoload -Uz vcs_info
+      precmd() { vcs_info }
+      setopt prompt_subst
 
-        # Customize the prompt
-        export PROMPT="${PWD##*/}@${:-%m}${vcs_info_msg_0_} ~ "
-      '';
+      # vcs_info の設定
+      zstyle ":vcs_info:git:*" formats " (%b)"
+      zstyle ":vcs_info:*" enable git
+
+      # プロンプト設定
+      export PROMPT="%F{blue}%~%f@%m%F{yellow}${vcs_info_msg_0_}%f ~ "
+      
+      # Zshオプション
+      setopt auto_menu
+      setopt auto_pushd
+      setopt extended_glob
+    '';
   };
 }
 
