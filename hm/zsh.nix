ef8bca3913d43c73ec824c90dd90d9a3ed33f62c
amwo <tsushimori@gmail.com>
2025-01-02 14:44:43 +0900

    init

--- hm/zsh.nix
+++ hm/zsh.nix
@@ -34,6 +34,17 @@
         export EDITOR="nvim"
         export NIXPKGS_ALLOW_UNFREE=1
         export TERM=xterm-256color
+	export PROMPT="${PWD##*/}@${:-%m}${vcs_info_msg_0_} ~ "
+
+	precmd() { vcs_info }
+
+	autoload -Uz compinit && compinit
+	autoload -Uz vcs_info
+
+	setopt PROMPT_SUBST
+
+	zstyle ":completion:*' matcher-list 'm:{a-z}={A-Z}"
+	zstyle ":vcs_info:git:*' formats ' (%b)"
       '';
   };
 }
