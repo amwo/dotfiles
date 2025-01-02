21b1c3e6fb8d9165afdcff3fb4c661f3f855d122
amwo <tsushimori@gmail.com>
2025-01-02 14:57:49 +0900

    init

--- hm/zsh.nix
+++ hm/zsh.nix
@@ -16,12 +16,12 @@
       v = "nvim";
 
       gs = "git status";
-      ls = "ls -a";
+      ls = "ls -lh";
       rl = "exec $SHELL -l";
       vi = "nvim";
       dc = "docker compose";
 
-      dfc = "df -h | grep '/dev'";
+      dfc = "df -h | grep '^/dev'";
       cat = "bat";
       vim = "nvim";
       dci = "docker run --rm -it";
@@ -34,14 +34,15 @@
         export EDITOR="nvim"
         export NIXPKGS_ALLOW_UNFREE=1
         export TERM=xterm-256color
-	export PROMPT="${PWD##*/}@${:-%m}${vcs_info_msg_0_} ~ "
+        export PROMPT="%F{blue}%~%f@%m%F{yellow}${vcs_info_msg_0_}%f ~ "
 
-	autoload -Uz compinit && compinit
-	autoload -Uz vcs_info
+        autoload -Uz compinit && compinit
+        autoload -Uz vcs_info
 
-	setopt PROMPT_SUBST
+        setopt PROMPT_SUBST
 
-	zstyle ":completion:*" matcher-list "m:{a-z}={A-Z}"
-	zstyle ":vcs_info:git:*" formats " (%b)"
+        zstyle ":completion:*" matcher-list "m:{a-z}={A-Z}"
+        zstyle ":vcs_info:git:*" formats " (%b)"
+      '';
   };
 }
